9fb738ffb5370a0734aadf607edd35e6cd835f2a
laszlobalint1987@gmail.com <laszlobalint1987@gmail.com>
2018-09-14 19:40:49 +0200

    Initialize new project from scratch

---
+++ SpringBoot/FirstTestProject/src/main/java/com/example/FirstTestProject/FirstTestProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.FirstTestProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstTestProjectApplication.class, args);
+	}
+}
