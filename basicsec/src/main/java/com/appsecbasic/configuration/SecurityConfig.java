915e6860ebe478b28a0932f20f66f37ecca20245
laszlobalint1987@gmail.com <laszlobalint1987@gmail.com>
2018-09-22 18:31:35 +0200

    Password encoding fixed

--- basicsec/src/main/java/com/appsecbasic/configuration/SecurityConfig.java
+++ basicsec/src/main/java/com/appsecbasic/configuration/SecurityConfig.java
@@ -1,10 +1,12 @@
-package configuration;
+package com.appsecbasic.configuration;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 
@@ -22,7 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 				.withUser("admin")
 				.password("secret")
-				.roles("ROLES");
+				.roles("ADMIN");
 	}
 
 	@Override
@@ -35,5 +37,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 				.formLogin()
 				.permitAll();
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Bean
+	public static NoOpPasswordEncoder passwordEncoder() {
+		return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
 	}
 }
--- basicsec/src/main/java/com/appsecbasic/controller/SecurityController.java
+++ basicsec/src/main/java/com/appsecbasic/controller/SecurityController.java
@@ -1,4 +1,4 @@
-package controller;
+package com.appsecbasic.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
